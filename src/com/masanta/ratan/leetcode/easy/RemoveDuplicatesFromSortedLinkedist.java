70bccb9e65ac522fcc2356ff9030b5edf73b59ac
Ratan Masanta <ratanmasanta@gmail.com>
2022-02-20 01:47:13 +0530

    Remove duplicates from a sorted linked list

---
+++ src/com/masanta/ratan/leetcode/easy/RemoveDuplicatesFromSortedLinkedist.java
@@ -0,0 +1,43 @@
+package com.masanta.ratan.leetcode.easy;
+
+public class RemoveDuplicatesFromSortedLinkedist {
+
+
+	private static class ListNode {
+		int val;
+		ListNode next;
+		ListNode() {}
+		ListNode(int val) { this.val = val; }
+		ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+	}
+
+	public static void main(String[] args) {
+		ListNode head = new ListNode(1);
+		head.next = new ListNode(1);
+		head.next.next = new ListNode(2);
+		head.next.next.next = new ListNode(3);
+		head.next.next.next.next = new ListNode(3);
+		
+		deleteDuplicates(head);
+		
+		while(head!=null) {
+			System.out.println(head.val);
+			head = head.next;
+		}
+		
+		System.out.println("Done");
+	}
+
+
+	public static ListNode deleteDuplicates(ListNode head) {
+		ListNode temp = head;
+		while(temp!=null && temp.next!=null )    
+		{
+			if(temp.val==temp.next.val)
+				temp.next=temp.next.next;
+			else
+				temp=temp.next;
+		}
+		return head;
+	}
+}
